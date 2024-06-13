6f7f4ec0c3aa13b82a86d6fe0e51617f783cef9c
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2024-06-13 07:53:17 -0400

    imported shoppingcart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import shoppingCart from "../assets/shopping-cart.png"; // Import the image
 function Navbar({ cartQuantity }) {
   return (
     <div className="navbar">
@@ -12,7 +13,7 @@ function Navbar({ cartQuantity }) {
           <Link to="shop">Shop</Link>
         </li>
         <li>
-          <img width="35px" src="src/assets/shopping-cart.png" alt="cart" />
+          <img width="35px" src={shoppingCart} alt="cart" />
         </li>
         <p className="cartQuantity">{cartQuantity}</p>
       </ul>
